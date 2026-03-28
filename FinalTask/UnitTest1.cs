8735791d45ea43ce5cb10d815e9178854576dc9d
SyndicateUA <Syndicate053@gmail.com>
2026-03-28 21:55:52 +0200

    Add project files.

---
+++ FinalTask/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalTask
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
