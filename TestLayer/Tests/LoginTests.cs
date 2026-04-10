a27e2d6e9e64147642f1bbb279edd91dd0c14b0f
SyndicateUA <Syndicate053@gmail.com>
2026-04-10 08:25:08 +0300

    Added multiple credential sets for TestCases

--- TestLayer/Tests/LoginTests.cs
+++ TestLayer/Tests/LoginTests.cs
@@ -21,6 +21,7 @@ namespace TestLayer.Tests
         }        
 
         [TestCase("test", "test")]
+        [TestCase("standard_user", "secret_sauce")]
         public void TestLoginFormWithEmptyCredentials(string username, string password)
         {
             LoginPage loginPage = new(Browser);
@@ -31,9 +32,10 @@ namespace TestLayer.Tests
             loginPage.ClearPassword();
             loginPage.PressLoginButton();
 
-            loginPage.GetErrorMessage().Should().Be("Epic sadface: Username is required");
+            loginPage.GetErrorMessage().Should().Be("Epic sadface: Username is required", because: "Error message Epic sadface: Username is required is expected because both username and password were cleared before attempting to login");
         }
 
+        [TestCase("test", "test")]
         [TestCase("standard_user", "secret_sauce")]
         public void TestLoginFormWithOnlyUsernameProvided(string username, string password)
         {
@@ -44,10 +46,11 @@ namespace TestLayer.Tests
             loginPage.ClearPassword();
             loginPage.PressLoginButton();
 
-            loginPage.GetErrorMessage().Should().Be("Epic sadface: Password is required");
+            loginPage.GetErrorMessage().Should().Be("Epic sadface: Password is required", because: "Error message Epic sadface: Password is required is expected because the password field was cleared before attempting to login");
         }
 
         [TestCase("standard_user", "secret_sauce")]
+        [TestCase("visual_user", "secret_sauce")]
         public void TestLoginFormWithValidCredentials(string username, string password)
         {
             LoginPage loginPage = new(Browser);
@@ -58,11 +61,11 @@ namespace TestLayer.Tests
 
             var inventoryPage = new InventoryPage(Browser);
 
-            inventoryPage.IsBurgerMenuDisplayed().Should().BeTrue();
-            inventoryPage.IsSwagLabsLabelDisplayed().Should().BeTrue();
-            inventoryPage.IsCartIconDisplayed().Should().BeTrue();
-            inventoryPage.IsFilterDropdownDisplayed().Should().BeTrue();
-            inventoryPage.AreInventoryItemsDisplayed().Should().BeTrue();
+            inventoryPage.IsBurgerMenuDisplayed().Should().BeTrue(because: "The burger menu should be visible because user logged in with valid credentials");
+            inventoryPage.IsSwagLabsLabelDisplayed().Should().BeTrue(because: "The Swag Labs label should be visible because user logged in with valid credentials");
+            inventoryPage.IsCartIconDisplayed().Should().BeTrue(because: "The cart icon should be visible because user logged in with valid credentials");
+            inventoryPage.IsFilterDropdownDisplayed().Should().BeTrue(because: "The filter dropdown should be visible because user logged in with valid credentials");
+            inventoryPage.AreInventoryItemsDisplayed().Should().BeTrue(because: "The inventory items should be listed because user logged in with valid credentials");
         }
 
         [TearDown]
